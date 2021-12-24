2d890a93d43013aa6fc2966b177b91df5dc0d4de
jaeho <jaeho3690@gmail.com>
2021-12-24 12:22:56 +0900

    first commit

--- dataset.py
+++ dataset.py
@@ -5,59 +5,114 @@ author baiyu
 import os
 import sys
 import pickle
+import numpy as np
 
-from skimage import io
 import matplotlib.pyplot as plt
-import numpy
 import torch
+import torchvision
 from torch.utils.data import Dataset
 
-class CIFAR100Train(Dataset):
-    """cifar100 test dataset, derived from
-    torch.utils.data.DataSet
-    """
+class ReMiniImageNet:
+    def __init__(self,args):
+        self.original_path = args.original_miniimagenet_path
+        self.modified_path = args.modified_miniimagenet_path
+    
+    def build(self):
+        modified_train, modified_test = self.reshuffle_miniimagenet()
+        self.save_to_modified_path(modified_train, modified_test)
 
-    def __init__(self, path, transform=None):
-        #if transform is given, we transoform data using
-        with open(os.path.join(path, 'train'), 'rb') as cifar100:
-            self.data = pickle.load(cifar100, encoding='bytes')
-        self.transform = transform
+
+    def save_to_modified_path(self, modified_train, modified_test):
+        """Save to modified path"""
+        with open(os.path.join(self.modified_path,'modified-mini-imagenet-train.pkl'), 'wb') as train_file:
+            pickle.dump(modified_train, train_file, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(os.path.join(self.modified_path,'modified-mini-imagenet-test.pkl'), 'wb') as test_file:
+            pickle.dump(modified_test, test_file, protocol=pickle.HIGHEST_PROTOCOL)
+
+        print('Saved both modified train, test to ',self.modified_path)
+
+    def reshuffle_miniimagenet(self):
+        self.id2numericallabel ={}
+        with open(os.path.join(self.original_path, "mini-imagenet-classlabel.pkl"), "rb") as file:
+            self.id2actuallabel = pickle.load(file)
+
+        for idx,(k,_) in enumerate(self.id2actuallabel.items()):
+            self.id2numericallabel[k] = idx 
+        
+        with open(os.path.join(self.original_path,'mini-imagenet-cache-train.pkl'), 'rb') as file:
+            original_train_data = pickle.load(file)
+        with open(os.path.join(self.original_path,'mini-imagenet-cache-val.pkl'), 'rb') as file:
+            original_val_data = pickle.load(file)
+        with open(os.path.join(self.original_path,'mini-imagenet-cache-test.pkl'), 'rb') as file:
+            original_test_data = pickle.load(file)
+
+        t_train_image, t_train_label, t_test_image, t_test_label = self.split_to_train_test(original_train_data)
+        v_train_image, v_train_label, v_test_image, v_test_label = self.split_to_train_test(original_val_data)
+        te_train_image, te_train_label, te_test_image, te_test_label = self.split_to_train_test(original_test_data)
+
+        modified_train_image = np.concatenate([t_train_image,v_train_image,te_train_image])
+        modified_train_label = t_train_label + v_train_label + te_train_label
+        modified_test_image = np.concatenate([t_test_image,v_test_image,te_test_image])
+        modified_test_label = t_test_label + v_test_label + te_test_label
+
+        modified_train = {'image_data':modified_train_image, 'class_label':modified_train_label}
+        modified_test = {'image_data':modified_test_image, 'class_label':modified_test_label}
+
+        return modified_train, modified_test
+
+
+
+    def split_to_train_test(self,data):
+        """There are 600 images per class. Use the first 480(80%) as train, rest as test"""
+        train_idx_list =[]
+        test_idx_list =[]
+        train_label =[]
+        test_label =[]
+
+
+        for k,v in data['class_dict'].items():
+            train_idx_list.extend(v[:480])
+            test_idx_list.extend(v[480:])
+            train_label.extend([self.id2numericallabel[k]]*480)
+            test_label.extend([self.id2numericallabel[k]]*120)
+        assert len(train_idx_list) == len(train_label)
+        assert len(test_idx_list) == len(test_label)
+        
+        train_image = data['image_data'][train_idx_list,:,:,:]
+        test_image = data['image_data'][test_idx_list,:,:,:]
+
+        return train_image, train_label, test_image, test_label
+    
+
+
+
+
+class MiniImageNet100(Dataset):
+    def __init__(self, mode, data_path):
+        self.modified_file = os.path.join(data_path,f'modified-mini-imagenet-{mode}.pkl')
+        with open(self.modified_file, 'rb') as file:
+            self.data = pickle.load(file)
+
+        # define dataset length
+        self.length = self.data['image_data'].shape[0]
+        # define transform module
+        self.transform = torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            ]
+        )
 
     def __len__(self):
-        return len(self.data['fine_labels'.encode()])
+        """Return number of total samples
+        Returns: number of total samples
+        """
+        return self.length
 
     def __getitem__(self, index):
-        label = self.data['fine_labels'.encode()][index]
-        r = self.data['data'.encode()][index, :1024].reshape(32, 32)
-        g = self.data['data'.encode()][index, 1024:2048].reshape(32, 32)
-        b = self.data['data'.encode()][index, 2048:].reshape(32, 32)
-        image = numpy.dstack((r, g, b))
+        image = self.data['image_data'][index,:,:,:]
+        label = self.data['class_label'][index]
 
         if self.transform:
             image = self.transform(image)
-        return label, image
-
-class CIFAR100Test(Dataset):
-    """cifar100 test dataset, derived from
-    torch.utils.data.DataSet
-    """
-
-    def __init__(self, path, transform=None):
-        with open(os.path.join(path, 'test'), 'rb') as cifar100:
-            self.data = pickle.load(cifar100, encoding='bytes')
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.data['data'.encode()])
-
-    def __getitem__(self, index):
-        label = self.data['fine_labels'.encode()][index]
-        r = self.data['data'.encode()][index, :1024].reshape(32, 32)
-        g = self.data['data'.encode()][index, 1024:2048].reshape(32, 32)
-        b = self.data['data'.encode()][index, 2048:].reshape(32, 32)
-        image = numpy.dstack((r, g, b))
-
-        if self.transform:
-            image = self.transform(image)
-        return label, image
-
+        return image,label
--- train.py
+++ train.py
@@ -23,6 +23,7 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
 from conf import settings
+from dataset import ReMiniImageNet
 from utils import get_network, get_training_dataloader, get_test_dataloader, WarmUpLR, \
     most_recent_folder, most_recent_weights, last_epoch, best_acc_weights
 
@@ -30,7 +31,7 @@ def train(epoch):
 
     start = time.time()
     net.train()
-    for batch_index, (images, labels) in enumerate(cifar100_training_loader):
+    for batch_index, (images, labels) in enumerate(miniimagenet_training_loader):
 
         if args.gpu:
             labels = labels.cuda()
@@ -42,7 +43,7 @@ def train(epoch):
         loss.backward()
         optimizer.step()
 
-        n_iter = (epoch - 1) * len(cifar100_training_loader) + batch_index + 1
+        n_iter = (epoch - 1) * len(miniimagenet_training_loader) + batch_index + 1
 
         last_layer = list(net.children())[-1]
         for name, para in last_layer.named_parameters():
@@ -56,7 +57,7 @@ def train(epoch):
             optimizer.param_groups[0]['lr'],
             epoch=epoch,
             trained_samples=batch_index * args.b + len(images),
-            total_samples=len(cifar100_training_loader.dataset)
+            total_samples=len(miniimagenet_training_loader.dataset)
         ))
 
         #update training loss for each iteration
@@ -83,7 +84,7 @@ def eval_training(epoch=0, tb=True):
     test_loss = 0.0 # cost function error
     correct = 0.0
 
-    for (images, labels) in cifar100_test_loader:
+    for (images, labels) in miniimagenet_test_loader:
 
         if args.gpu:
             images = images.cuda()
@@ -103,44 +104,50 @@ def eval_training(epoch=0, tb=True):
     print('Evaluating Network.....')
     print('Test set: Epoch: {}, Average loss: {:.4f}, Accuracy: {:.4f}, Time consumed:{:.2f}s'.format(
         epoch,
-        test_loss / len(cifar100_test_loader.dataset),
-        correct.float() / len(cifar100_test_loader.dataset),
+        test_loss / len(miniimagenet_test_loader.dataset),
+        correct.float() / len(miniimagenet_test_loader.dataset),
         finish - start
     ))
     print()
 
     #add informations to tensorboard
     if tb:
-        writer.add_scalar('Test/Average loss', test_loss / len(cifar100_test_loader.dataset), epoch)
-        writer.add_scalar('Test/Accuracy', correct.float() / len(cifar100_test_loader.dataset), epoch)
+        writer.add_scalar('Test/Average loss', test_loss / len(miniimagenet_test_loader.dataset), epoch)
+        writer.add_scalar('Test/Accuracy', correct.float() / len(miniimagenet_test_loader.dataset), epoch)
 
-    return correct.float() / len(cifar100_test_loader.dataset)
+    return correct.float() / len(miniimagenet_test_loader.dataset)
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-net', type=str, required=True, help='net type')
-    parser.add_argument('-gpu', action='store_true', default=False, help='use gpu or not')
-    parser.add_argument('-b', type=int, default=128, help='batch size for dataloader')
-    parser.add_argument('-warm', type=int, default=1, help='warm up training phase')
-    parser.add_argument('-lr', type=float, default=0.1, help='initial learning rate')
-    parser.add_argument('-resume', action='store_true', default=False, help='resume training')
+    parser.add_argument('--net', type=str, default='squeezenet', help='net type')
+    parser.add_argument('--gpu', action='store_true', default=False, help='use gpu or not')
+    parser.add_argument('--b', type=int, default=128, help='batch size for dataloader')
+    parser.add_argument('--warm', type=int, default=1, help='warm up training phase')
+    parser.add_argument('--lr', type=float, default=0.1, help='initial learning rate')
+    parser.add_argument('--resume', action='store_true', default=False, help='resume training')
+    parser.add_argument('--original_miniimagenet_path',type=str, default ='data/miniimagenet')
+    parser.add_argument('--modified_miniimagenet_path',type=str, default ='data/modified_miniimagenet')
     args = parser.parse_args()
 
     net = get_network(args)
 
+    if not os.path.isfile(os.path.join(args.modified_miniimagenet_path,'modified-mini-imagenet-train.pkl')):
+        os.mkdir(args.modified_miniimagenet_path)
+        Reconstruct = ReMiniImageNet(args)
+        Reconstruct.build()
+    
+
     #data preprocessing:
-    cifar100_training_loader = get_training_dataloader(
-        settings.CIFAR100_TRAIN_MEAN,
-        settings.CIFAR100_TRAIN_STD,
+    miniimagenet_training_loader = get_training_dataloader(
+        path = 'data/modified_miniimagenet/',
         num_workers=4,
         batch_size=args.b,
         shuffle=True
     )
 
-    cifar100_test_loader = get_test_dataloader(
-        settings.CIFAR100_TRAIN_MEAN,
-        settings.CIFAR100_TRAIN_STD,
+    miniimagenet_test_loader = get_test_dataloader(
+        path = 'data/modified_miniimagenet/',
         num_workers=4,
         batch_size=args.b,
         shuffle=True
@@ -149,7 +156,7 @@ if __name__ == '__main__':
     loss_function = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
     train_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=settings.MILESTONES, gamma=0.2) #learning rate decay
-    iter_per_epoch = len(cifar100_training_loader)
+    iter_per_epoch = len(miniimagenet_training_loader)
     warmup_scheduler = WarmUpLR(optimizer, iter_per_epoch * args.warm)
 
     if args.resume:
@@ -170,7 +177,7 @@ if __name__ == '__main__':
     #so the only way is to create a new tensorboard log
     writer = SummaryWriter(log_dir=os.path.join(
             settings.LOG_DIR, args.net, settings.TIME_NOW))
-    input_tensor = torch.Tensor(1, 3, 32, 32)
+    input_tensor = torch.Tensor(1, 3, 256, 256)
     if args.gpu:
         input_tensor = input_tensor.cuda()
     writer.add_graph(net, input_tensor)
